083dfc4f427f262b2b6b64282e373926f0275c9c
vanshikasharma798379-hub <vanshikasharma798379@gmail.com>
2026-07-26 15:38:43 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    int id;
+    String name;
+    int age;
+    String course;
+    public Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+    public void display() {
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Course : " + course);
+    }
+}
